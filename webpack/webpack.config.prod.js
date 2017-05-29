e03dc3a1367882ff3deaaaf2fc9bd1af35baa00e
Ahmed <ahmedalkuhlani@gmail.com>
2017-05-29 16:22:26 +0800

    production env settings

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -31,11 +31,6 @@ module.exports = {
     ],
   },
   plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production'),
-      },
-    }),
     new webpack.optimize.UglifyJsPlugin(),
     new webpack.optimize.CommonsChunkPlugin({
       names: ['vendor', 'manifest'],
